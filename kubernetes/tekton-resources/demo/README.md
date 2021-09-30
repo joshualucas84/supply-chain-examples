24dad6c9d99b8fb46d92016bed87d7007f3d670f
Joshua Lucas <joshua.lucas@outlook.com>
2021-09-30 18:37:43 -0500

    checkin for abstracting the install process for gatekeeper-demo

--- kubernetes/tekton-resources/demo/README.md
+++ kubernetes/tekton-resources/demo/README.md
@@ -2,6 +2,9 @@
 
 * Generate your own keys, and replace the public keys in admission-control-verify-image.yaml for gcr.io and ttl.sh
 
+##TODO add moreinfo about replacing keys in various files in developer-patches
+
+
 ## Run build
 
 For the sake of simplicity run `update_and_run.sh`. This makes it easy to apply any changes you might make to the pipeline for testing.
--- kubernetes/tekton-resources/demo/update-and-run.sh
+++ kubernetes/tekton-resources/demo/update-and-run.sh
@@ -1,44 +1,25 @@
-#kubectl apply -f admission-control-verify-image.yaml
-#kubectl apply -f gatekeeper-signing-checker-resources.yaml
-#kubectl apply -f gatekeeper-signing-checker.yaml
-#kubectl apply -f gatekeeper-constraints-template.yaml
-#kubectl apply -f gatekeeper-constraints.yaml
-#kubectl apply -f kaniko-with-cosign.yaml
-#kubectl apply -f pipeline.yaml
-#kubectl create -f pipeline-run.yaml
 #!/usr/bin/env bash
 set -u
 set -e
 
-remove_old_helm_charts(){
-    rm gatekeeper-demo-*.tgz || true
-}
-
-
-remove_old_template_charts() {
-    kubectl delete -f gatekeeper-constraints-template.yaml || true
-    kubectl delete -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/demo/basic/templates/k8srequiredlabels_template.yaml || true
-}
-
-build_charts() {
-    helm package . --debug
+remove_old_charts() {
+    kubectl delete -k . || true
+    ##TODO constraints need time for template CRD's to start before they can be applied
+    kubectl delete -f constraint-templates/gatekeeper-constraints.yaml || true
 }
 
 install_new_charts() {
-    ## Install gatekeeper helm charts
-    kubectl apply -f gatekeeper-constraints-template.yaml || true
-    kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/demo/basic/templates/k8srequiredlabels_template.yaml || true
-    ## Install
-    helm install -f values.yaml gatekeeper-demo-*.tgz --generate-name || true
+    kubectl apply -k . || true
+    echo "Wait 5 seconds for gatekeeper-constraint-templates to become available"
+    sleep 5
+    ##TODO constraints need time for template CRD's to start before they can be applied
+    kubectl apply -f constraint-templates/gatekeeper-constraints.yaml || true
 }
 
 
 main(){
-    remove_old_helm_charts "$@"
-    remove_old_template_charts "$@"
-    build_charts "$@"
+    remove_old_charts "$@"
     install_new_charts "$@"
-    remove_old_helm_charts "$@"
 }
 
 main "$@"
